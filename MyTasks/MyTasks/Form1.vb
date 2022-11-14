23f0ff27c8abcb1ce350bb2e1ce93176b416fed7
yassine <menssouri2811@gmail.com>
2022-11-14 21:14:51 +0100

    Commit inicial: creacion de la aplicacion

---
+++ MyTasks/MyTasks/Form1.vb
@@ -0,0 +1,6 @@
+﻿Imports System.Configuration
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim connection = ConfigurationManager.AppSettings.Get("connection")
+    End Sub
+End Class
